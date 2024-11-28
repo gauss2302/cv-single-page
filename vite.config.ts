471aac4a379f1e5d3fb84949dfd10d3fdd95c8ad
gauss2302 <shilov6865@gmail.com>
2024-11-28 16:04:56 +0500

    vite and package.json fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,16 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/my-cv/", // Make sure this matches your GitHub repository name
+  base: "/cv-single-page/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    copyPublicDir: true,
+  },
 });
